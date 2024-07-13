0b1bc3376636b54627c81db29078f777ed580d3f
Ramazan Bilen <ramazan10bilen@gmail.com>
2024-07-13 14:04:51 +0300

    web_f

--- Day 05/web/web.py
+++ Day 05/web/web.py
@@ -32,6 +32,26 @@ def register():
         mydb.close()
         return redirect(url_for('index'))
 
+#update code
+cursor = mydb.cursor()
+username = "ali"
+email = "veli@gmail.com"
+password = '123asd'
+userid = 4
+
+cursor.execute("UPDATE users SET username = %s, email = %s, password = %s WHERE userid = %s",(username, email, password,userid))
+mydb.commit()
+
+
+#delete code
+cursor2 = mydb.cursor()
+userid = 2
+
+cursor2.execute('DELETE FROM users WHERE userid = %s ', (userid,))
+mydb.commit()
+
+mydb.close()
+
 if __name__=='__main__':
     app.run(debug=True)   
      
